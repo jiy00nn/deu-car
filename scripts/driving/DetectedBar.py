aa48ca67a7104c57865c7fa04370654e398b71cd
jiy00nn <wldbs204@gmail.com>
2021-11-29 14:55:00 +0000

    feature: add pick line and detected bar

--- scripts/driving/DetectedBar.py
+++ scripts/driving/DetectedBar.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-# BEGIN ALL
 import cv2
 import numpy
 import cv_bridge
@@ -8,14 +7,15 @@ from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Image
 
 class DetectedBar:
-    def __init__(self):  # creator Detector_Bar
+    def __init__(self):
+        rospy.init_node("detected_bar")
         self.bridge = cv_bridge.CvBridge()
-        # cv2.namedWindow("window", 1)
-        self.image_sub = rospy.Subscriber('camera/rgb/image_raw', Image, self.image_callback)
         self.cmd_vel_pub = rospy.Publisher('cmd_vel_mux/input/teleop', Twist, queue_size=1)
-        self.bar_pub = rospy.Publisher('camera/rgb/image_raw/p2_bar', Image, queue_size=1)  # detect blockbar
+        self.image_sub = rospy.Subscriber('camera/rgb/image_raw', Image, self.image_callback)
+        self.bar_pub = rospy.Publisher('camera/rgb/image_raw/p2_bar', Image, queue_size=1)
         self.twist = Twist()
-        self.linecount=0
+        self.bar_signal = False
+        self.start_time = rospy.Time.now()
 
     def image_callback(self, msg):
         barimage = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
@@ -31,16 +31,35 @@ class DetectedBar:
         mask[0:h, 0:250] = 0
 
         M = cv2.moments(mask)
-        self.twist.linear.x = 0.8
+        self.bar_signal = True
         if M['m00'] > 0:
             cx = int(M['m10'] / M['m00'])
             cy = int(M['m01'] / M['m00'])
             cv2.circle(barimage, (cx, cy), 10, (255, 0, 0), -1)
-            # BGR = RED
-            # BEGIN CONTROL
-            self.twist.linear.x = 0.0
-            # END CONTROL
-            rospy.loginfo('detecting bar...')
+            self.bar_signal = False
         self.cmd_vel_pub.publish(self.twist)
         bar_image_msg = self.bridge.cv2_to_imgmsg(barimage, 'bgr8')
-        self.bar_pub.publish(bar_image_msg)  # publish
+        self.bar_pub.publish(bar_image_msg)
+
+    def go_forward(self):
+        self.twist.linear.x = 0.8
+        self.cmd_vel_pub.publish(self.twist)
+
+    def stop(self):
+        self.twist.linear.x = 0.0
+        self.cmd_vel_pub.publish(self.twist)
+
+    def loop(self):
+        while not rospy.is_shutdown():
+            if self.bar_signal == True:
+                self.go_forward()
+                if (rospy.Time.now() - self.start_time) > rospy.Duration.from_sec(10):
+                    break
+            else:
+                self.stop()
+
+
+if __name__=='__main__':
+    rospy.init_node("detected_bar")
+    detected_bar = DetectedBar()
+    detected_bar.loop()
